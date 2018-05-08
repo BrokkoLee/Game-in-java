447f647842753456a10a190289ee12ed288168a2
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 13:03:09 +0200

    Added introduction

--- Functions.java
+++ Functions.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Functions {
+    public static void introduction(Computer player1, Computer player2, boolean tokenResult){
+        System.out.println("The two players are:" + player1.name + "and" + player2.name);
+        if (tokenResult) {
+            System.out.println("Based on the coin, " + player1.name + " is starting the round.");
+        }else{
+            System.out.println("Based on the coin, " + player2.name + " is starting the round.");
+        }
+        System.out.println();
+    }
+
+    
 }
