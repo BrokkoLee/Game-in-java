682153ec17538b5092f190350aa06fae34b1f3f5
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 13:11:36 +0200

    Removed dice method, import dice from functions

--- Computer.java
+++ Computer.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 import java.util.stream.*;
-import java.util.Arrays;
+
 
 public class Computer {
     public String name;
     public int[] numbers;
     public int[] threw;
     public boolean result;
+
 
     public void Computer(){
         name = name;
@@ -15,9 +16,6 @@ public class Computer {
         result = result;
     }
 
-    int dice(){
-        return(int )(Math.random() * 6 + 1);
-    }
 
     public void thrown(){
         int sum = IntStream.of(numbers).sum();
@@ -26,7 +24,7 @@ public class Computer {
         int chosenRound = (int )(Math.random() * 3 + 1);
         int bit = 0;
         for (int i = 1; i <= chosenRound; i++){
-            int diceNumber = dice();
+            int diceNumber = Functions.dice();
             this.threw[bit] = diceNumber;
             this.numbers[bit] = diceNumber;
             bit++;
