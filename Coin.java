e7840f130082e75f719d719a70cf429d3b09c770
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 12:18:55 +0200

    Added coin constructor

--- Coin.java
+++ Coin.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Coin {
+    boolean bool;
+
+    public Coin(){
+        int random = (int )(Math.random()*0 + 1);
+        if (random == 0){
+            bool = true;
+        } else {
+            bool = false;
+        }
+    }
 }
