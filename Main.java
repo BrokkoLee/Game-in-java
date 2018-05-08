0d623340b715ab8719d44120afd458e5a1039791
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 13:14:48 +0200

    Added 2 computers

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         Coin token = new Coin();
+        Computer computerOne = new Computer();
+        Computer computerTwo = new Computer();
     }
 }
