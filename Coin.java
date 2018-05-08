98fa9b1b8bee25dfbe4b7ec8f7cce1c6c892c738
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 12:24:12 +0200

    Changed random

--- Coin.java
+++ Coin.java
@@ -2,8 +2,8 @@ public class Coin {
     boolean bool;
 
     public Coin(){
-        int random = (int )(Math.random()*0 + 1);
-        if (random == 0){
+        int random = (int )(Math.random() * 2) + 1;
+        if (random == 1){
             bool = true;
         } else {
             bool = false;
