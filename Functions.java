05a07ab7d16b002260c5f01c20894aa3aa0886b6
Levente Wolf <wlevi2002@gmail.com>
2018-05-08 13:09:29 +0200

    Added draw function

--- Functions.java
+++ Functions.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 public class Functions {
     public static void introduction(Computer player1, Computer player2, boolean tokenResult){
         System.out.println("The two players are:" + player1.name + "and" + player2.name);
@@ -9,5 +11,16 @@ public class Functions {
         System.out.println();
     }
 
-    
+    public void draw(int gameRound, Computer player1, Computer player2){
+        int numbersSum1 = IntStream.of(player1.numbers).sum();
+        int numbersSum2 = IntStream.of(player2.numbers).sum();
+        if (gameRound == 2 && numbersSum1 < 21 && numbersSum2 < 21){
+            if (numbersSum1 > numbersSum2){
+                System.out.println(player1.name + " has just won the game with " + numbersSum1 + " points.");
+            } else {
+                System.out.println(player2.name + " has just won the game with " + numbersSum2 + " points.");
+            }
+        }
+    }
+
 }
