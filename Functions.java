aec88d111a2441e3e908716d9dcbc34ee8bcb251
Levente Wolf <wlevi2002@gmail.com>
2018-05-09 20:46:54 +0200

    Changed blank line outpring

--- Functions.java
+++ Functions.java
@@ -2,11 +2,10 @@ public class Functions {
     public static void introduction(Computer player1, Computer player2, boolean tokenResult){
         System.out.println("The two players are: " + player1.name + " and " + player2.name);
         if (tokenResult) {
-            System.out.println("Based on the coin, " + player1.name + " is starting the round.");
+            System.out.println("Based on the coin, " + player1.name + " is starting the round.\n");
         }else{
-            System.out.println("Based on the coin, " + player2.name + " is starting the round.");
+            System.out.println("Based on the coin, " + player2.name + " is starting the round.\n");
         }
-        System.out.println();
     }
 
     public static void draw(int gameRound, Computer player1, Computer player2){
