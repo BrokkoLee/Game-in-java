8e229c6d5bedd397f1fde62993fe5a353cd4162a
Levente Wolf <wlevi2002@gmail.com>
2018-05-09 19:50:52 +0200

    Added Array sour function

--- OutputHandler.java
+++ OutputHandler.java
@@ -1,2 +1,11 @@
 public class OutputHandler {
+    public static void ArraySout(int[] array){
+        for(int i = 0; i < array.length; i++){
+            if (array[i] == 0){
+                break;
+            } else {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
 }
