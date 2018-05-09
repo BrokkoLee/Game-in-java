b70fbab5296dc4728a6f66117e7fc10bd0a59e4c
Levente Wolf <wlevi2002@gmail.com>
2018-05-09 20:44:51 +0200

    no message

--- Functions.java
+++ Functions.java
@@ -1,5 +1,3 @@
-import java.util.stream.IntStream;
-
 public class Functions {
     public static void introduction(Computer player1, Computer player2, boolean tokenResult){
         System.out.println("The two players are: " + player1.name + " and " + player2.name);
