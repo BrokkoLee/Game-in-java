b24948f4e177434fd540b61b46be84b963f8e46c
Levente Wolf <wlevi2002@gmail.com>
2018-05-10 21:54:11 +0200

    Changed result name

--- Main.java
+++ Main.java
@@ -5,26 +5,26 @@ public class Main{
                 new Computer("Computer_One"),
                 new Computer("Computer_Two")
         };
-        
+
         Functions.introduction(computers, computers.length, coinResult);
 
         for (int game = 1; game <= 3; game++) {
             if (coinResult) {
                 computers[0].thrown();
-                if (computers[0].result || computers[1].result) {
+                if (computers[0].isWon || computers[1].isWon) {
                     break;
                 }
                 computers[1].thrown();
-                if (computers[0].result || computers[1].result) {
+                if (computers[0].isWon || computers[1].isWon) {
                     break;
                 }
             } else {
                 computers[1].thrown();
-                if (computers[0].result || computers[1].result) {
+                if (computers[0].isWon || computers[1].isWon) {
                     break;
                 }
                 computers[0].thrown();
-                if (computers[0].result || computers[1].result) {
+                if (computers[0].isWon || computers[1].isWon) {
                     break;
                 }
             }
