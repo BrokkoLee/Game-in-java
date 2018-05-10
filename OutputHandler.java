6f0e4bcb3470043b14aa3af24134443e042c4ac2
Levente Wolf <wlevi2002@gmail.com>
2018-05-10 18:12:27 +0200

    Removed ArraySout function

--- OutputHandler.java
+++ OutputHandler.java
@@ -1,11 +1,3 @@
 public class OutputHandler {
-    public static void ArraySout(int[] array){
-        for(int i = 0; i < array.length; i++){
-            if (array[i] == 0){
-                break;
-            } else {
-                System.out.print(array[i] + " ");
-            }
-        }
-    }
+    
 }
