b3f94c04590c3b3c265f6687e8d430bece9e4ced
Levente Wolf <wlevi2002@gmail.com>
2018-05-11 20:39:13 +0200

    Used gameMaster

--- Main.java
+++ Main.java
@@ -5,31 +5,9 @@ public class Main{
                 new Computer("Computer_One"),
                 new Computer("Computer_Two")
         };
-
+        GameMaster gameMaster = new GameMaster(computers, 3);
         Functions.introduction(computers, computers.length, coinResult);
 
-        for (int game = 1; game <= 3; game++) {
-            if (coinResult) {
-                computers[0].thrown();
-                if (computers[0].isWon || computers[1].isWon) {
-                    break;
-                }
-                computers[1].thrown();
-                if (computers[0].isWon || computers[1].isWon) {
-                    break;
-                }
-            } else {
-                computers[1].thrown();
-                if (computers[0].isWon || computers[1].isWon) {
-                    break;
-                }
-                computers[0].thrown();
-                if (computers[0].isWon || computers[1].isWon) {
-                    break;
-                }
-            }
-            Functions.draw(game, computers[0], computers[1]);
-        }
-
+        gameMaster.handleGame(coinResult);
     }
 }
