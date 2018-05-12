c22ddcb75aadd46bd48c7b4101cee73044de1e5b
Levente Wolf <wlevi2002@gmail.com>
2018-05-12 14:37:47 +0200

    Created rounds

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main{
     public static void main(String[] args) {
         boolean coinResult = Coin.throwCoin();
+        int rounds = 3;
         Computer[] computers = new Computer[]{
                 new Computer("Computer_One"),
                 new Computer("Computer_Two")
         };
-        GameMaster gameMaster = new GameMaster(computers, 3);
+        GameMaster gameMaster = new GameMaster(computers, rounds);
         Functions.introduction(computers, computers.length, coinResult);
 
         gameMaster.handleGame(coinResult);
