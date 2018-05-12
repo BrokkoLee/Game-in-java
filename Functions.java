628245917c2b878eb0978ad6384fbc6952077d28
Levente Wolf <wlevi2002@gmail.com>
2018-05-12 15:59:16 +0200

    Removed introduction from functions, added to outputhanlder

--- Functions.java
+++ Functions.java
@@ -1,15 +1,4 @@
 public class Functions {
-    public static void introduction(Computer[] computers, int computerArrayLength, boolean tokenResult){
-        System.out.println("The players are: ");
-        for (int i = 0; i < computerArrayLength; i++){
-            System.out.print(computers[i].name + " ");
-        }
-        if (tokenResult) {
-            System.out.println("\nBased on the coin, " + computers[0].name + " is starting the round.\n");
-        }else{
-            System.out.println("\nBased on the coin, " + computers[1].name + " is starting the round.\n");
-        }
-    }
     public static void draw(int currentRound, int lastRound, Computer[] computers){
         if (currentRound == lastRound-1){
             for (Computer computer:computers){
@@ -32,7 +21,6 @@ public class Functions {
         }
         return max;
     }
-    
     public static int sum(int[] Array){
         int result = 0;
         for (int number : Array ){
