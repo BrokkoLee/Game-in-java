0f135246160a3e6442bf1053cac0766902d525d7
Levente Wolf <wlevi2002@gmail.com>
2018-05-12 18:32:13 +0200

    Added playersname sout

--- OutputHandler.java
+++ OutputHandler.java
@@ -1,13 +1,16 @@
 public class OutputHandler {
     public static void introduction(Computer[] computers, int computerArrayLength, boolean tokenResult){
-        System.out.println("The players are: ");
-        for (int i = 0; i < computerArrayLength; i++){
-            System.out.print(computers[i].name + " ");
-        }
+        soutPlayers(computers, computerArrayLength);
         if (tokenResult) {
             System.out.println("\nBased on the coin, " + computers[0].name + " is starting the round.\n");
         }else{
             System.out.println("\nBased on the coin, " + computers[1].name + " is starting the round.\n");
+        }
+    }
+    private static void soutPlayers(Computer[] computers, int computerArrayLength){
+        System.out.println("The players are: ");
+        for (int i = 0; i < computerArrayLength; i++){
+            System.out.print(computers[i].name + " ");
         }
     }
 
