83694346c502c9e252290fc6b143a834e14d440f
Levente Wolf <wlevi2002@gmail.com>
2018-05-12 14:38:29 +0200

    Fixed game end, added draw function

--- GameMaster.java
+++ GameMaster.java
@@ -17,18 +17,21 @@ public class GameMaster {
     }
 
     public void handleGame(boolean coinResult){
+        mainLoop:
         for(int i = 0; i < numberOfRounds; i++){
             if (coinResult){
                 for (int j = 0; j < computers.length; j++){     //for each
                     computers[j].thrown();
-                    if (isGameOver()) {break;}
+                    if (isGameOver()) {break mainLoop;}
                 }
             }else{
                 for (int k = computers.length-1; k >= 0; k--){  //for each?
                     computers[k].thrown();
-                    if (isGameOver()) {break;}
+                    if (isGameOver()) {break mainLoop;}
                 }
             }
+
+            Functions.draw(i,numberOfRounds-1, computers[0], computers[1]);
         }
     }
 
