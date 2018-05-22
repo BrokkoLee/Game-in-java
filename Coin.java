d7bd7bcbe3c84fafa963c68aa044c3817e54b315
Levente Wolf <wlevi2002@gmail.com>
2018-05-22 21:38:53 +0200

    Shorted return statement

--- Coin.java
+++ Coin.java
@@ -1,10 +1,6 @@
 public class Coin {
     public static boolean throwCoin(){
         int random = (int )(Math.random() * 2) + 1;
-        if (random == 1){
-            return true;
-        } else {
-            return false;
-        }
+        return random == 1;
     }
 }
