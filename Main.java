8d7e2352746a7c4eacc8ac2d25b0a06f53a52878
Levente Wolf <wlevi2002@gmail.com>
2018-05-25 20:21:50 +0200

    Used more computers

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@ public class Main{
     public static void main(String[] args) {
         boolean coinResult = Coin.throwCoin();
         int rounds = 3;
-        Computer[] computers = new Computer[]{
-                new Computer("Computer_One"),
-                new Computer("Computer_Two")
-        };
+        int maxComputers = 5;
+        Computer[] computers = new Computer[maxComputers];
+
+        for (int i = 0; i < maxComputers; i++) {
+            computers[i] = GameMaster.inputComputer();
+        }
+
         GameMaster gameMaster = new GameMaster(computers, rounds);
         OutputHandler.introduction(computers,coinResult);
         gameMaster.handleGame(coinResult);
