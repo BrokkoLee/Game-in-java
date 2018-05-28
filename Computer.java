27de2458b6ea26cc3c9a0967aa60449cabe82015
Levente Wolf <wlevi2002@gmail.com>
2018-05-28 20:49:38 +0200

    Changed playDice function to non-static

--- Computer.java
+++ Computer.java
@@ -12,20 +12,20 @@ public class Computer {
     public void PlayRound(){
         OutputHandler.showCurrentScore(name, numbers);
 
-        playDice(numbers, bit);
+        playDice(numbers);
 
         OutputHandler.showNewScore(numbers);
     }
 
-    public static void playDice(int[] numbers, int counter){
+    public void playDice(int[] numbers){
         int numberOfDices = Dice.chooseNumberOfDice();
         System.out.println("He decided to throw " + numberOfDices + " times.");
         System.out.print("The result of the throw(s): ");
         for (int i = 1; i <= numberOfDices; i++){
             int diceResult = new Dice(6).throwDice();
             System.out.print(diceResult + " ");
-            numbers[counter] = diceResult;
-            counter++;
+            numbers[bit] = diceResult;
+            bit++;
         }
     }
 }
