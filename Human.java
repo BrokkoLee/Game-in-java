043ed6dad5d38a50431e65cd0e1dfb777d79f2c6
Levente Wolf <wlevi2002@gmail.com>
2018-06-03 20:14:11 +0200

    Added chooseNumberOfDice function to human

--- Human.java
+++ Human.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Human extends Player{
     public Human(String name) {
         super(name);
+    }
+
+    public static int chooseNumberOfDice(){
+        Scanner reader = new Scanner(System.in);
+        OutputHandler.showScanInNumberOfDices();
+        int numberOfDices = reader.nextInt();
+        return numberOfDices;
     }
 }
