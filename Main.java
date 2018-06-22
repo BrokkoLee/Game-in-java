a0c5ad0d4d24144a7747bc80598231dc4cf63c99
Levente Wolf <wlevi2002@gmail.com>
2018-06-22 14:17:47 +0200

    Scanned variables from file

--- Main.java
+++ Main.java
@@ -3,15 +3,18 @@ import java.util.ArrayList;
 public class Main{
     public static void main(String[] args) {
         //TODO get variable value from file
-        int rounds = 5;
-        int numberOfComputers = 3;
-        int numberOfHumans = 1;
+        ArrayList<String> valueList;
+        valueList = FileReader.getValueFromFile();
+        final int round = Integer.parseInt(valueList.get(1));
+        final int goalPoint = Integer.parseInt(valueList.get(3));
+        final int computerPlayers = Integer.parseInt(valueList.get(5));
+        final int humanPlayers = Integer.parseInt(valueList.get(7));
 
         ArrayList<Player> players = new ArrayList<>();
 
-        GameMaster.inputPlayers(players, numberOfComputers, numberOfHumans);
+        GameMaster.inputPlayers(players, computerPlayers, humanPlayers);
 
-        GameMaster gameMaster = new GameMaster(players, rounds);
+        GameMaster gameMaster = new GameMaster(players, round);
 
         gameMaster.playGame();
     }
